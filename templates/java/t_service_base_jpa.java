d89b252d70229533bd285490b801c4063c674076
杨柳 <yangliu817@126.com>
2019-05-30 13:51:10 +0800

    更新 t_service_base_jpa.java

--- templates/java/t_service_base_jpa.java
+++ templates/java/t_service_base_jpa.java
@@ -2,17 +2,11 @@
 [package]
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
+import javax.persistence.TypedQuery;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -84,52 +78,12 @@ public interface JpaService<T, ID extends Serializable> {
     List<T> findAll(@Nullable Specification<T> spec, Sort sort);
 
     long count(@Nullable Specification<T> spec);
+	
+	Specification<T> buildSpecification(Object queryPojo);
 
-    default Specification<T> build(T queryPojo) {
-        return (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-            if (queryPojo != null) {
+    T getOneByJpql(T entity);
 
-                Class<?> clazz = queryPojo.getClass();
-                List<Field> fieldList = new ArrayList<>();
-                while (!clazz.equals(Object.class)) {
-                    Field[] fields = clazz.getDeclaredFields();
-                    if (fields.length > 0) {
-                        fieldList.addAll(Arrays.asList(fields));
-                    }
-                    clazz = clazz.getSuperclass();
-                }
+    List<T> listByJpql(T entity);
 
-                for (Field field : fieldList) {
-                    if (field.isSynthetic() || Modifier.isStatic(field.getModifiers())) {
-                        continue;
-                    }
-                    String fieldName = field.getName();
-                    boolean accessible = field.isAccessible();
-                    if (!accessible) {
-                        field.setAccessible(true);
-                    }
-                    Object value = null;
-                    try {
-                        value = field.get(queryPojo);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e.getMessage(),e);
-                    }
-
-                    if (!accessible) {
-                        field.setAccessible(false);
-                    }
-                    if (Objects.isNull(value)) {
-                        continue;
-                    }
-                    Path<?> path = root.get(fieldName);
-
-                    Predicate predicate = cb.equal(path, value);
-                    predicates.add(predicate);
-                }
-
-            }
-            return cb.and(predicates.toArray(new Predicate[]{}));
-        };
-    }
+    TypedQuery<?> buildJpqlQuery(Object queryPojo, Class<?> clazz);
 }
